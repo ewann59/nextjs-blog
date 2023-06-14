54d3b170524ac430187da9c264214c3bd17ad899
Ewann59 <Ewannroels59@gmail.com>
2023-06-14 10:47:46 +0200

    modif

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,20 @@
 import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
+import styles from '../styles/Home.module.css';
+import Link from 'next/link';
+import Image from 'next/image';
+
+const YourComponent = () => (
+  <Image
+    src="/images/profile.jpg" // Chemin du fichier image
+    height={144} // Taille souhaitée avec le bon ratio d'aspect
+    width={144} // Taille souhaitée avec le bon ratio d'aspect
+    alt="Your Name"
+  />
+);
+
 
 export default function Home() {
   return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-    </Layout>
-  );
-}
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
@@ -126,5 +123,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  
-
+  )
+}
